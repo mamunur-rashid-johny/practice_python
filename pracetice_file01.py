18ebcbdd99c8ced89fcecf58b8e7e4958f43f74b
Mamunur Rashid <johny.ice10@gmail.com>
2020-02-25 16:44:59 +0600

    update file again

--- pracetice_file01.py
+++ pracetice_file01.py
@@ -3,3 +3,8 @@ for key,value in my_dict.items():
     print(key)
     print(value)
 my_list = ['jimmy','reza','sohag','nayeem']
+a = len(my_list)
+while(a>0):
+    print("my name is "+my_list[a-1])
+    a -=1
+
